38fe4172bab21a4bc71d14d3d707711b5f0aba3c
Robert Chen <robchen401@gmail.com>
2013-07-09 19:04:34 +0000

    modified web.js to read in index.html

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var fs = require('fs');
+//var index = 
 
 var app = express.createServer(express.logger());
 
+/*
+fs.readFileSync('./index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+*/
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2! modified 13:40 on July 9 2013!');
+    //response.send('Hello World 2! modified 13:40 on July 9 2013!');
+    //use: fs.readFileSync(filename, [options])
+    response.send(fs.readFileSync('./index.html'));
 });
 
 var port = process.env.PORT || 5000;
